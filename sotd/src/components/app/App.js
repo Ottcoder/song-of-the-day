cec176646159286db4ba5fdc6533b168e6bfe880
OttCoder <101673336+Ottcoder@users.noreply.github.com>
2022-12-05 10:33:21 +0000

    Basic data added and tested app runs. Next step is to display hard coded music data in a viewable format before moving onto using a database

---
+++ sotd/src/components/app/App.js
@@ -0,0 +1,12 @@
+import Heading from '../heading/Heading';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Heading/>
+    </div>
+  );
+}
+
+export default App;
---
+++ sotd/src/components/heading/Heading.js
@@ -0,0 +1,9 @@
+import './Heading.css'
+
+function Heading() {
+    return (
+        <h1 className='Title'>Song of the day!</h1>
+    );
+}
+
+export default Heading
